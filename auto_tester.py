6b43fcd58c658bd328d12582917c6414c4a63705
James Petersen <jpetersenames@gmail.com>
2018-04-02 20:31:01 -0400

    changes to auto_tester

--- auto_tester.py
+++ auto_tester.py
@@ -8,18 +8,15 @@
 #   school year.
 #
 
-# Imports
-#from oauth2client.service_account import ServiceAccountCredentials
-#import gspread
-import time
+# Imports..................
 from   ctypes import *
-import spidev
+from   sys    import argv
 import RPi.GPIO as g
 import datetime
 import serial
+import spidev
 import time
 import csv
-from sys import argv
 
 # Variables
 acs_offset = 2.438
@@ -31,7 +28,7 @@ curnt_col  = 3
 temp_col   = 4
 
 print argv
-if len(argv) < 1:
+if len(argv) > 1:
     print len(argv)
     print argv[1]
 
@@ -45,53 +42,44 @@ vl = CDLL('./vl6180/vl6180.so')
 # Get the TMP007 C functions
 tm = CDLL('./tmp007/tmp007.so')
 
-# use creds to create a client to interact with the Google Drive API
-#scope = ['https://spreadsheets.google.com/feeds']
-#creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-#client = gspread.authorize(creds)
-
-# Find a workbook by name and open the first sheet
-#sheet = client.open("autotest").sheet1
 
 # This function returns the 10-bit integer
 # received from the ADC.
 def get_adc(channel):
     if ((channel > 1) or (channel < 0)):
         return -1
-    r = spi.xfer2([1,(2+channel)<<6,0])
-    ret = ((r[1]&31) << 6) + (r[2] >> 2)
+    r = spi.xfer2([1, (2 + channel) << 6, 0])
+    ret = ((r[1] & 31) << 6) + (r[2] >> 2)
     return ret
 
 
 # This function returns the converted
 # adc voltage
 def adc_to_volt(reading):
-    return float((reading*5.0)/1024)
+    return float((reading * 5.0) / 1024)
 
+
+# This function gets the measured voltage
+# from the ADC and returns the converted voltage
 def get_voltage():
     reading = 0
-    for i in xrange(7):
+    for i in xrange(63):
         reading = reading + get_adc(1)
-    reading = reading>>3
-    return adc_to_volt(reading)
+    reading = reading>>6
+    return (adc_to_volt(reading) * 5.0)
 
 
 # This function returns the oversampled
 # current value from the ACS712
 def get_current():
     reading = 0
-    for i in xrange(7):
+    for i in xrange(63):
         reading = reading + get_adc(0)
-    reading = reading>>3
-    a2d_volt = adc_to_volt(reading)
-    adj_volt = (a2d_volt - acs_offset)
-    cur = adj_volt / acs_step
-    #print "Reading:     ", reading
-    #print "A2D Voltage: ", a2d_volt
-    #print "Adjust Volt: ", adj_volt
-    #print "Current:     ", cur
-    #print "---------------------"
-    return cur
+    reading  = reading>>6
+    adc_volt = adc_to_volt(reading)
+    adj_volt = adc_volt - acs_offset
+    current  = adj_volt / acs_step
+    return current
 
 
 # This function initializes the VL6180 sensor.
@@ -109,69 +97,65 @@ def init_vl6180():
     g.output(gpin, 1)
     return vl.vl6180_setup()
 
-
+force_on = False
 # Main loop.........
 try:
 # This function reads from rs232 
-#    ser = serial.Serial(
-#        port='/dev/ttyUSB0',
-#        baudrate = 19200,
-#        parity=serial.PARITY_NONE,
-#        stopbits=serial.STOPBITS_ONE,
-#        bytesize=serial.EIGHTBITS,
-#        timeout=1
-#    )
-#    counter=0
+    if (force_on):
+        ser = serial.Serial(
+            port     = '/dev/ttyUSB0',
+            baudrate = 19200,
+            parity   = serial.PARITY_NONE,
+            stopbits = serial.STOPBITS_ONE,
+            bytesize = serial.EIGHTBITS,
+            timeout  = 1
+        )
+        print "Opening " + str(ser.name) + " for load cell communication."
 
-
+    # Initialize the VL6180
     vl6180_fd = init_vl6180()
 
-    tmp007_fd = tm.tmp007_setup()
+    # Initialize the TMP007 and set up for Python use
+    tmp007_fd   = tm.tmp007_setup()
     tmp007_read = tm.tmp007_read_temp
     tmp007_read.restype = c_float
 
-#    sheet.update_cell(row, time_col,  "Time (h:m:s)")
-#    sheet.update_cell(row, dist_col,  "Distance (mm)")
-#    sheet.update_cell(row, curnt_col, "Current (A)")
-#    sheet.update_cell(row, temp_col,  "Temperature (C)")
-
+    # Get the time and create file name
     timestr = time.strftime("%m-%d-%y_%H-%M-%S")
-    print timestr
-    filename = "tests/" + timestr + "_file.csv"
+    filename = "tests/" + timestr + "_test.csv"
 
     with open(filename, 'wb') as outfile:
         csv_w = csv.writer(outfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         csv_w.writerow(['Time (h:m:s)', 'Current [A]', 'Voltage [V]', 'Distance [mm]', 'Temperature [C]', 'Force [Units]'])
+
         while True:
+
+            # Get the measurements
+            if (force_on):
+                force       = ser.readline()
+                mylist = force.split()
+                print mylist[0]
+                print mylist[1]
+                print mylist[2]
             acs_current = get_current()
             volt        = get_voltage()
             dist        = vl.vl6180_read_range(vl6180_fd)
             temp        = float(tmp007_read(tmp007_fd))
             time        = str(datetime.datetime.now().time())
-            csv_w.writerow([time, acs_current, volt, dist, temp, "force"])
 
-            # Get the distance measurement
-#            print "distance:      " + str(dist)   + " mm"
-#            print "temperature:   " + str(temp)   + " C"
-#            print "voltage:       " + str(volt)   + " V [regulated]"
-#            print "voltage [tot]: " + str(volt*5) + " V [actual]"
-#            print "-------------------"
-
-        # Increment the row 
-#        x=ser.readline()
-#        print x
-#        row += 1
-        # Update the sheet
-
-#sheet.update_cell(row, time_col, str(datetime.datetime.now().time()))
-#sheet.update_cell(row, curnt_col, acs_current)
-#sheet.update_cell(row, dist_col, dist)
-#sheet.update_cell(row, temp_col, temp)
+            # Write to csv
+            if (force_on):
+                csv_w.writerow([time, acs_current, volt, dist, temp, force])
+            else:
+                csv_w.writerow([time, acs_current, volt, dist, temp, "force"])
 
 
-
-
-
+            print "distance:    " + str(dist)  + " mm"
+            print "temperature: " + str(temp)  + " C"
+            print "voltage:     " + str(volt)  + " V"
+            if (force_on):
+                print "force:       " + str(force) + " N"
+            print "-------------------"
 
 # Close up the spi..
 finally:
